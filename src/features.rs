6bdfe7c97ccaac970e0083feaeef1c65c7e0f3a3
Parvesh Barak <pbarak@login16.chn.perlmutter.nersc.gov>
2025-12-28 03:09:47 -0800

    taking coverage in accoutn while deciding if a site is informative

--- src/features.rs
+++ src/features.rs
@@ -18,6 +18,8 @@ use crate::pbars::PBarNotification;
 use crate::windowing::{extract_windows, OverlapWindow};
 
 pub(crate) const TOP_K: usize = 20;
+const MIN_COV_TH: u32 = 18;
+
 
 const BASE_LOWER: [u8; 128] = [
     255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
@@ -727,15 +729,18 @@ where
             ins = 0;
         }
 
+        let mut cov = 0;
         counter.iter_mut().for_each(|(_, c)| *c = 0);
         col.iter().for_each(|&b| {
             if b == b'.' {
                 return;
             }
 
-            // if b == b'*' || b == b'#' {
-            //     return; // skip indels
-            // }
+            cov += 1;
+
+            if b == b'*' || b == b'#' {
+                return; // skip indels
+            }
 
             *counter.get_mut(&BASE_FORWARD[b as usize]).unwrap() += 1;
         });
@@ -743,7 +748,7 @@ where
         let n_supported = counter
             .iter()
             .fold(0u8, |acc, (_, &c)| if c >= 3 { acc + 1 } else { acc });
-        if module != "consensus" && n_supported >= 2 {
+        if module != "consensus" && n_supported >= 2 && cov >= MIN_COV_TH {
             supporeted.push(SupportedPos::new(tpos as u16, ins));
         }
     }
